ff15439c16aca87dd6c6506b8b2ff81599778e19
aj-white <72359843+aj-white@users.noreply.github.com>
2020-11-13 10:31:08 +0000

    initial commit

--- problem_set_1/mario/mario_less.c
+++ problem_set_1/mario/mario_less.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(void)
+{
+    // Get height from user
+    int height;
+    do
+    {
+        printf("Height: ");
+        scanf("%i", &height);
+    }
+    while (height < 1 || height > 8);
+
+    // Draw pyramid
+    for (int row = 1; row <= height; row += 1)
+    {
+        // Number of spaces printed is height - row
+        printf("%*s", height - row, "");
+        for (int column = 0;column < row; column += 1)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
